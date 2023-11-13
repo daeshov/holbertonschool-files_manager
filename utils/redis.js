bb67469f7e046bf927a73a5f1a23688840123ae9
mnlazs <mnl.azs@gmail.com>
2023-11-13 11:35:56 -0600

    updated

---
+++ utils/redis.js
@@ -0,0 +1,49 @@
+const Redis = require('redis'); // Correr "npm list redis" para asegurarme de tener instalada la biblioteca Redis
+
+class RedisClient {
+  constructor() {
+    this.client = Redis.createClient();
+
+    // Manejar errores de conexión
+    this.client.on('error', (err) => {
+      console.error(`Error en la conexión Redis: ${err}`);
+    });
+  }
+
+  async isAlive() {
+    // Verificar si la conexión está viva
+    return await new Promise((resolve) => {
+      this.client.ping('pong', (err) => {
+        resolve(!err);
+      });
+    });
+  }
+
+  async get(key) {
+    return await new Promise((resolve) => {
+      this.client.get(key, (err, value) => {
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return await new Promise((resolve) => {
+      this.client.setex(key, duration, value, (err) => {
+        resolve(!err);
+      });
+    });
+  }
+
+  async del(key) {
+    return await new Promise((resolve) => {
+      this.client.del(key, (err) => {
+        resolve(!err);
+      });
+    });
+  }
+}
+
+// Crear e exportar una instancia de RedisClient llamada redisClient
+const redisClient = new RedisClient();
+module.exports = redisClient;
