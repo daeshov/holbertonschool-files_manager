7c9dd2966bd3019a52271182c89e50b343c4880a
mnlazs <mnl.azs@gmail.com>
2023-11-13 15:59:02 -0600

    up

--- utils/db.js
+++ utils/db.js
@@ -78,13 +78,3 @@ class DBClient {
 const dbClient = new DBClient();
 
 module.exports = dbClient;
-
-// DBClient should have:
-// the constructor that creates a client to MongoDB:
-// host: from the environment variable DB_HOST or default: localhost
-// port: from the environment variable DB_PORT or default: 27017
-// database: from the environment variable DB_DATABASE or default: files_manager
-// a function isAlive that returns true when the connection to MongoDB is a success otherwise, false
-// an asynchronous function nbUsers that returns the number of documents in the collection users
-// an asynchronous function nbFiles that returns the number of documents in the collection files
-// After the class definition, create and export an instance of DBClient called dbClient.
