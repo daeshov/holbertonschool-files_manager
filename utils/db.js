48020aaa244e99d02aa73113f87be5e286ebfe08
mnlazs <mnl.azs@gmail.com>
2023-11-13 15:10:26 -0600

    updated

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,8 @@
-// importa el paquete mongodb y configura las variables de entorno.
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb');
 
-const {
-  DB_HOST = 'localhost',
-  DB_PORT = 27017,
-  DB_DATABASE = 'files_manager',
-} = process.env;
-
-// Creacion de DBCliente con sus variables de entorno
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const dbName = process.env.DB_DATABASE || 'files_manager';
 
 class DBClient {
   constructor() {
@@ -27,28 +22,67 @@ class DBClient {
   isAlive() {
     return !!this.db;
   }
-  async nbUsers() {
-    try {
-      const usersCollection = this.db.collection('users');
-      const userCount = await usersCollection.countDocuments();
-      return userCount;
-    } catch (error) {
-      console.error('Error getting user count:', error.message);
-      return -1; // O cualquier valor que indique un error
-    }
+
+  async nbUsers() { return this.db.collection('users').countDocuments(); }
+
+  async nbFiles() { return this.db.collection('files').countDocuments(); }
+
+  async findUser(user) { return this.db.collection('users').findOne(user); }
+
+  async createUser(email, password) {
+    await this.db.collection('users').insertOne({ email, password });
+
+    const newUser = await this.db.collection('users').findOne({ email });
+
+    return { id: newUser._id, email };
   }
 
-  async nbFiles() {
-    try {
-      const filesCollection = this.db.collection('files');
-      const fileCount = await filesCollection.countDocuments();
-      return fileCount;
-    } catch (error) {
-      console.error('Error getting file count:', error.message);
-      return -1; // O cualquier valor que indique un error
-    }
+  async uploadFile(data) {
+    await this.db.collection('files').insertOne(data);
+    return this.db.collection('files').findOne(data);
+  }
+
+  async findFile(data) {
+    return this.db.collection('files').findOne(data);
+  }
+
+  async aggregateFiles(userId, parentId, page = 1) {
+    const user = { userId };
+    if (parentId && parentId !== undefined) user.parentId = parentId;
+
+    const cursor = await this.db.collection('files').aggregate([
+      { $match: user },
+      { $skip: (page - 1) * 20 },
+      { $limit: 20 },
+    ]).toArray();
+    const files = [];
+    cursor.forEach(({
+      _id, userId, name, type, isPublic, parentId,
+    }) => {
+      files.push({
+        id: _id, userId, name, type, isPublic, parentId,
+      });
+    });
+    return files;
+  }
+
+  async updateFile(data, change) {
+    await this.db.collection('files').updateOne(data, { $set: change });
+    return this.db.collection('files').findOne(data);
   }
 }
 
 const dbClient = new DBClient();
+
 module.exports = dbClient;
+
+
+//DBClient should have:
+// the constructor that creates a client to MongoDB:
+// host: from the environment variable DB_HOST or default: localhost
+// port: from the environment variable DB_PORT or default: 27017
+// database: from the environment variable DB_DATABASE or default: files_manager
+// a function isAlive that returns true when the connection to MongoDB is a success otherwise, false
+// an asynchronous function nbUsers that returns the number of documents in the collection users
+// an asynchronous function nbFiles that returns the number of documents in the collection files
+// After the class definition, create and export an instance of DBClient called dbClient.
