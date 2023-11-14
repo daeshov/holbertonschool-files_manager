d1affe4411b65e8f00d66c3d61a920acc06fce52
mnlazs <mnl.azs@gmail.com>
2023-11-14 13:32:00 -0600

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+/*
 //Importacion 
 import { postNew, getMe } from '../controllers/UsersController';
 
@@ -18,3 +19,31 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UserController, postNew);
 
 module.exports = router;
+*/
+
+
+import express from 'express';
+
+import { getStatus, getStats } from '../controllers/AppController';
+import { postNew, getMe } from '../controllers/UsersController';
+import { getConnect, getDisconnect } from '../controllers/AuthController';
+import {
+  postUpload, getShow, getIndex, putPublish, putUnPublish, getFile,
+} from '../controllers/FilesController';
+
+const router = express.Router();
+
+router.get('/status', getStatus);
+router.get('/stats', getStats);
+router.post('/users', postNew);
+router.get('/users/me', getMe);
+router.get('/connect', getConnect);
+router.get('/disconnect', getDisconnect);
+router.post('/files', postUpload);
+router.get('/files', getIndex);
+router.get('/files/:id', getShow);
+router.put('/files/:id/publish', putPublish);
+router.put('/files/:id/unpublish', putUnPublish);
+router.get('/files/:id/data', getFile);
+
+export default router;
