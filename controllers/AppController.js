7c50c3d409bb269117b8d7b5a56ee3baa1fac65a
mnlazs <mnl.azs@gmail.com>
2023-11-14 13:26:13 -0600

    updated

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,3 +1,4 @@
+/*
 // aqui abajo estamos usando las funciones de utilidad
 const {
   getRedisStatus, getDBStatus, countUsers, countFiles,
@@ -33,4 +34,24 @@ const AppController = {
   },
 };
 
+*/
+
 module.exports = AppController;
+
+
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export const getStatus = (req, res) => {
+  const redis = redisClient.isAlive();
+  const db = dbClient.isAlive();
+
+  res.send({ redis, db });
+};
+
+export const getStats = async (req, res) => {
+  const users = await dbClient.nbUsers();
+  const files = await dbClient.nbFiles();
+
+  res.send({ users, files });
+};
