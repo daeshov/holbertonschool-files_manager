93b2dd43647e2165b4d028311835ae2d0f2b9f28
darah thomas <Jdarahthomas@gmail.com>
2023-11-14 12:42:56 -0600

    task 3 userscontroller.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UserController = require('../controllers/UsersController');
 
 //Ruta para obtener el estado
 router.get('/status', AppController.getStatus);
 
 //Ruta para obtener estadisticas
 router.get('/stats', AppController.getStats);
+
+// Route to creeate new user
+router.post('/users', UserController, postNew);
 
 module.exports = router;
