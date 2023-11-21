8888d6151320f314881313d60ccc8f2d223933c5
darah thomas <Jdarahthomas@gmail.com>
2023-11-21 15:54:44 -0600

    task 3 updated

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,25 +6,21 @@ import dbClient from '../utils/db';
 export const postNew = async (req, res) => {
   const { email, password } = req.body;
 
-  if (!email) return res.status(400).json({ error: 'Missing email' });
-  if (!password) return res.status(400).json({ error: 'Missing password' });
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' });
+  }
 
-  let user = await dbClient.findUser({ email });
-  if (user) return res.status(400).json({ error: 'Already exist' });
+  if (!password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
 
-  user = await dbClient.createUser(email, sha1(password));
+  // Check if the email already exists in the database
+  const existingUser = await dbClient.findUser({ email });
+  if (existingUser) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
 
-  return res.json(user);
-};
+  const user = await dbClient.createUser(email, hashedPassword);
 
-export const getMe = async (req, res) => {
-  const token = req.header('X-token');
-
-  const uid = await RedisClient.get(`auth_${token}`);
-
-  if (!uid) return res.status(401).json({ error: 'Unauthorized' });
-
-  const user = await dbClient.findUser({ _id: ObjectId(uid) });
-
-  return res.json({ email: user.email, id: user._id });
+  return res.status(201).json({ email: user.email, id: user._id });
 };
